f927302f80e4da045be7b6e4de3539fe6c8d8567
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-08-23 12:51:11 +0500

    fix cors demo

--- examples/cors.js
+++ examples/cors.js
@@ -3,19 +3,15 @@ const cors = require('../node_modules/cors');
 
 const app = nanoexpress();
 
-var whitelist = ['http://localhost:5000'];
+// const whitelist = ['http://localhost:5000'];
 const corsConfigured = cors({
-  origin(origin, callback) {
-    if (origin && whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'http://localhost:5000',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 });
 
-app.use(corsConfigured);
+app.options('/*', corsConfigured);
 
 app.get('/', async () => ({ hello: 'world' }));
 app.post('/cors', async () => {
