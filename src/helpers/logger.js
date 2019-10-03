52061f3f8391a0b78688a5d6409e4b586ce0df23
Max Inno <maksym.hubach@gmail.com>
2019-10-03 20:30:15 +0300

    completely rewritten environment checks

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -8,8 +8,20 @@ const error = createDebug('nanoexpress:error');
 error.namespace = `[${error.namespace}]`;
 error.color = 1;
 
-log.enabled = error.enabled =
-  !process.env.NODE_ENV ||
-  (process.env.NODE_ENV && process.env.NODE_ENV === 'development');
+// Environment checks
+if (!process.env.DEBUG) {
+  // DEBUG passed -> default library flow
+  // DEBUG not passed -> check own directive
+  if (process.env.NANOEXPRESS_LOGGING) {
+    // Extract logging levels
+    const levels = process.env.NANOEXPRESS_LOGGING.split(',');
+    // Enable whitelisted levels
+    log.enabled = levels.includes('log');
+    error.enabled = levels.includes('error');
+  } else {
+    // Development environment -> enable all
+    log.enabled = error.enabled = process.env.NODE_ENV === 'development';
+  }
+}
 
 export default { log, error };
