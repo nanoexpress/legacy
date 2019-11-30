5c516a0e274047b09b6a21cf0a1f8217c0c78709
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-11-30 14:50:19 +0500

    improve performance

--- examples/basic.js
+++ examples/basic.js
@@ -2,14 +2,8 @@ const nanoexpress = require('..');
 
 const app = nanoexpress();
 
-app.get(
-  '/',
-  {
-    isRaw: true
-  },
-  (req, res) => {
-    res.end('hello world');
-  }
-);
+app.get('/', (req, res) => {
+  res.end('hello world');
+});
 
 app.listen(4000);
---
+++ src/compilers/index.js
@@ -0,0 +1,3 @@
+import Route from './route';
+
+export { Route };
